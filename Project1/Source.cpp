3e2ee849597ea62198a39f57e3c3eb4745ed15cd
NikitaAxenov <asounia.wr@mail.ru>
2024-04-11 20:23:56 +0700

    Создан шаблон программы-меню для 4 заданий

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,5 +3,52 @@
 using namespace std;
 
 int main() {
+	int task;
 
+	while (1) {
+		cout << "Select a task:" << endl;
+		cout << "1) Number of vowel letters in a string;" << endl;
+		cout << "2) Change the case of string letters;" << endl;
+		cout << "3) Removing punctuation marks;" << endl;
+		cout << "4) Palindrome test." << endl;
+		cout << "Enter the task number: ";
+
+		cin >> task;
+
+		switch (task)
+		{
+		case 1:
+		{
+			
+
+			break;
+		}
+		case 2:
+		{
+			
+
+			break;
+		}
+		case 3:
+		{
+			
+
+			break;
+		}
+		case 4:
+		{
+			
+
+			break;
+		}
+		default:
+		{
+			cout << "Error! Please enter the correct value!" << endl;
+
+			break;
+		}
+		}
+		cout << "---------------------------------------" << endl;
+	}
+	return 0;
 }
