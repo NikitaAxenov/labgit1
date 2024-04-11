48fc5d790ede82912e65a273dacc3d2c5591b510
NikitaAxenov <asounia.wr@mail.ru>
2024-04-11 20:33:26 +0700

    Добавлена функция, которая подсчитывает количество гласных букв в строке

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+void NumberOfVowels(string str1, int& res) {
+	char all_vowels[10] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+	for (int i = 0; i < str1.length(); i++)
+	{
+		for (int j = 0; j < 10; j++) {
+			if (str1[i] == all_vowels[j])
+				res++;
+		}
+	}
+}
+
+
+
 int main() {
 	int task;
 
@@ -19,7 +33,17 @@ int main() {
 		{
 		case 1:
 		{
-			
+			int result = 0;
+			string string1;
+
+			cout << "Enter your string: ";
+
+			cin.get();
+			getline(cin, string1);
+
+			NumberOfVowels(string1, result);
+
+			cout << "Vowels: " << result << endl;
 
 			break;
 		}
